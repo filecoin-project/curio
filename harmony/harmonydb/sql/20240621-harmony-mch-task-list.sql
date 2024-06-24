9bbdb30713add60a3329ef405a5b8c77607afad7
Andrew Jackson (Ajax) <snadrus@gmail.com>
2024-06-23 23:12:39 -0500

    sql2

--- harmony/harmonydb/sql/20240621-harmony-mch-task-list.sql
+++ harmony/harmonydb/sql/20240621-harmony-mch-task-list.sql
@@ -3,6 +3,6 @@ CREATE TABLE harmony_task_bid (
     id SERIAL PRIMARY KEY NOT NULL,
     task_id INTEGER NOT NULL,
     bid FLOAT NOT NULL,
-    bidder TIMESTAMP NOT NULL
+    bidder TIMESTAMP NOT NULL,
     update_time TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
