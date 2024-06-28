6bcf56a048c5532bc1fde747a2fe00d0f5ffe612
Łukasz Magiera <magik6k@gmail.com>
2024-06-28 16:27:58 +0200

    better decode

--- lib/proof/porep_vproof_bin_decode.go
+++ lib/proof/porep_vproof_bin_decode.go
@@ -10,60 +10,54 @@ func DecodeCommit1OutRaw(r io.Reader) (Commit1OutRaw, error) {
 	var out Commit1OutRaw
 	var err error
 
-	if out.CommD, err = DecodeCommitment(r); err != nil {
-		return out, err
-	}
-	if out.CommR, err = DecodeCommitment(r); err != nil {
-		return out, err
-	}
-	if out.RegisteredProof, err = DecodeStringRegisteredProofType(r); err != nil {
-		return out, err
-	}
-	if out.ReplicaID, err = DecodeCommitment(r); err != nil {
-		return out, err
-	}
-	if out.Seed, err = DecodeTicket(r); err != nil {
-		return out, err
-	}
-	if out.Ticket, err = DecodeTicket(r); err != nil {
-		return out, err
-	}
-
-	// Decode VanillaProofs
-	numProofTypes, err := binary.ReadUvarint(r.(io.ByteReader))
-	if err != nil {
-		return out, err
-	}
-
+	// VanillaProofs
 	out.VanillaProofs = make(map[StringRegisteredProofType][][]VanillaStackedProof)
-	for i := uint64(0); i < numProofTypes; i++ {
-		proofType, err := DecodeStringRegisteredProofType(r)
-		if err != nil {
-			return out, err
+	var vpOuterLength uint64
+	if err = binary.Read(r, binary.LittleEndian, &vpOuterLength); err != nil {
+		return out, fmt.Errorf("failed to read VanillaProofs outer length: %w", err)
+	}
+
+	for i := uint64(0); i < vpOuterLength; i++ {
+		var vpInnerLength uint64
+		if err = binary.Read(r, binary.LittleEndian, &vpInnerLength); err != nil {
+			return out, fmt.Errorf("failed to read VanillaProofs inner length: %w", err)
 		}
 
-		numPartitions, err := binary.ReadUvarint(r.(io.ByteReader))
-		if err != nil {
-			return out, err
-		}
-
-		partitions := make([][]VanillaStackedProof, numPartitions)
-		for j := uint64(0); j < numPartitions; j++ {
-			numChallenges, err := binary.ReadUvarint(r.(io.ByteReader))
+		proofs := make([]VanillaStackedProof, vpInnerLength)
+		for j := uint64(0); j < vpInnerLength; j++ {
+			proofs[j], err = DecodeVanillaStackedProof(r)
 			if err != nil {
-				return out, err
+				return out, fmt.Errorf("failed to decode VanillaStackedProof: %w", err)
 			}
-
-			challenges := make([]VanillaStackedProof, numChallenges)
-			for k := uint64(0); k < numChallenges; k++ {
-				challenges[k], err = DecodeVanillaStackedProof(r)
-				if err != nil {
-					return out, err
-				}
-			}
-			partitions[j] = challenges
 		}
-		out.VanillaProofs[proofType] = partitions
+
+		key := StringRegisteredProofType(fmt.Sprintf("Partition%d", i))
+		out.VanillaProofs[key] = [][]VanillaStackedProof{proofs}
+	}
+
+	// CommR
+	if out.CommR, err = DecodeCommitment(r); err != nil {
+		return out, fmt.Errorf("failed to decode CommR: %w", err)
+	}
+
+	// CommD
+	if out.CommD, err = DecodeCommitment(r); err != nil {
+		return out, fmt.Errorf("failed to decode CommD: %w", err)
+	}
+
+	// ReplicaID
+	if out.ReplicaID, err = DecodeCommitment(r); err != nil {
+		return out, fmt.Errorf("failed to decode ReplicaID: %w", err)
+	}
+
+	// Seed
+	if out.Seed, err = DecodeTicket(r); err != nil {
+		return out, fmt.Errorf("failed to decode Seed: %w", err)
+	}
+
+	// Ticket
+	if out.Ticket, err = DecodeTicket(r); err != nil {
+		return out, fmt.Errorf("failed to decode Ticket: %w", err)
 	}
 
 	return out, nil
@@ -74,30 +68,30 @@ func DecodeVanillaStackedProof(r io.Reader) (VanillaStackedProof, error) {
 	var err error
 
 	if out.CommDProofs, err = DecodeMerkleProof[Sha256Domain](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode CommDProofs: %w", err)
 	}
 	if out.CommRLastProof, err = DecodeMerkleProof[PoseidonDomain](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode CommRLastProof: %w", err)
 	}
 	if out.ReplicaColumnProofs, err = DecodeReplicaColumnProof[PoseidonDomain](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode ReplicaColumnProofs: %w", err)
 	}
 
-	numLabelingProofs, err := binary.ReadUvarint(r.(io.ByteReader))
-	if err != nil {
-		return out, err
+	var numLabelingProofs uint64
+	if err = binary.Read(r, binary.LittleEndian, &numLabelingProofs); err != nil {
+		return out, fmt.Errorf("failed to read number of LabelingProofs: %w", err)
 	}
 	out.LabelingProofs = make([]LabelingProof[PoseidonDomain], numLabelingProofs)
 	for i := uint64(0); i < numLabelingProofs; i++ {
 		out.LabelingProofs[i], err = DecodeLabelingProof[PoseidonDomain](r)
 		if err != nil {
-			return out, err
+			return out, fmt.Errorf("failed to decode LabelingProof: %w", err)
 		}
 	}
 
 	out.EncodingProof, err = DecodeEncodingProof[PoseidonDomain](r)
 	if err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode EncodingProof: %w", err)
 	}
 
 	return out, nil
@@ -109,7 +103,7 @@ func DecodeMerkleProof[H HasherDomain](r io.Reader) (MerkleProof[H], error) {
 
 	out.Data, err = DecodeProofData[H](r)
 	if err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode ProofData: %w", err)
 	}
 
 	return out, nil
@@ -121,7 +115,7 @@ func DecodeProofData[H HasherDomain](r io.Reader) (ProofData[H], error) {
 
 	proofType, err := binary.ReadUvarint(r.(io.ByteReader))
 	if err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to read proof type: %w", err)
 	}
 
 	switch proofType {
@@ -138,7 +132,11 @@ func DecodeProofData[H HasherDomain](r io.Reader) (ProofData[H], error) {
 		return out, fmt.Errorf("unknown proof type: %d", proofType)
 	}
 
-	return out, err
+	if err != nil {
+		return out, fmt.Errorf("failed to decode proof: %w", err)
+	}
+
+	return out, nil
 }
 
 func DecodeSingleProof[H HasherDomain](r io.Reader) (SingleProof[H], error) {
@@ -146,13 +144,13 @@ func DecodeSingleProof[H HasherDomain](r io.Reader) (SingleProof[H], error) {
 	var err error
 
 	if out.Root, err = DecodeHasherDomain[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Root: %w", err)
 	}
 	if out.Leaf, err = DecodeHasherDomain[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Leaf: %w", err)
 	}
 	if out.Path, err = DecodeInclusionPath[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Path: %w", err)
 	}
 
 	return out, nil
@@ -163,16 +161,16 @@ func DecodeSubProof[H HasherDomain](r io.Reader) (SubProof[H], error) {
 	var err error
 
 	if out.BaseProof, err = DecodeInclusionPath[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode BaseProof: %w", err)
 	}
 	if out.SubProof, err = DecodeInclusionPath[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode SubProof: %w", err)
 	}
 	if out.Root, err = DecodeHasherDomain[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Root: %w", err)
 	}
 	if out.Leaf, err = DecodeHasherDomain[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Leaf: %w", err)
 	}
 
 	return out, nil
@@ -183,19 +181,19 @@ func DecodeTopProof[H HasherDomain](r io.Reader) (TopProof[H], error) {
 	var err error
 
 	if out.BaseProof, err = DecodeInclusionPath[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode BaseProof: %w", err)
 	}
 	if out.SubProof, err = DecodeInclusionPath[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode SubProof: %w", err)
 	}
 	if out.TopProof, err = DecodeInclusionPath[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode TopProof: %w", err)
 	}
 	if out.Root, err = DecodeHasherDomain[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Root: %w", err)
 	}
 	if out.Leaf, err = DecodeHasherDomain[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Leaf: %w", err)
 	}
 
 	return out, nil
@@ -207,14 +205,14 @@ func DecodeInclusionPath[H HasherDomain](r io.Reader) (InclusionPath[H], error) 
 
 	numElements, err := binary.ReadUvarint(r.(io.ByteReader))
 	if err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to read number of elements: %w", err)
 	}
 
 	out.Path = make([]PathElement[H], numElements)
 	for i := uint64(0); i < numElements; i++ {
 		out.Path[i], err = DecodePathElement[H](r)
 		if err != nil {
-			return out, err
+			return out, fmt.Errorf("failed to decode PathElement: %w", err)
 		}
 	}
 
@@ -227,20 +225,20 @@ func DecodePathElement[H HasherDomain](r io.Reader) (PathElement[H], error) {
 
 	numHashes, err := binary.ReadUvarint(r.(io.ByteReader))
 	if err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to read number of hashes: %w", err)
 	}
 
 	out.Hashes = make([]H, numHashes)
 	for i := uint64(0); i < numHashes; i++ {
 		out.Hashes[i], err = DecodeHasherDomain[H](r)
 		if err != nil {
-			return out, err
+			return out, fmt.Errorf("failed to decode HasherDomain: %w", err)
 		}
 	}
 
 	out.Index, err = binary.ReadUvarint(r.(io.ByteReader))
 	if err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to read index: %w", err)
 	}
 
 	return out, nil
@@ -251,30 +249,30 @@ func DecodeReplicaColumnProof[H HasherDomain](r io.Reader) (ReplicaColumnProof[H
 	var err error
 
 	if out.C_X, err = DecodeColumnProof[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode C_X: %w", err)
 	}
 
 	numDrgParents, err := binary.ReadUvarint(r.(io.ByteReader))
 	if err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to read number of DRG parents: %w", err)
 	}
 	out.DrgParents = make([]ColumnProof[H], numDrgParents)
 	for i := uint64(0); i < numDrgParents; i++ {
 		out.DrgParents[i], err = DecodeColumnProof[H](r)
 		if err != nil {
-			return out, err
+			return out, fmt.Errorf("failed to decode DRG parent: %w", err)
 		}
 	}
 
 	numExpParents, err := binary.ReadUvarint(r.(io.ByteReader))
 	if err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to read number of EXP parents: %w", err)
 	}
 	out.ExpParents = make([]ColumnProof[H], numExpParents)
 	for i := uint64(0); i < numExpParents; i++ {
 		out.ExpParents[i], err = DecodeColumnProof[H](r)
 		if err != nil {
-			return out, err
+			return out, fmt.Errorf("failed to decode EXP parent: %w", err)
 		}
 	}
 
@@ -286,10 +284,10 @@ func DecodeColumnProof[H HasherDomain](r io.Reader) (ColumnProof[H], error) {
 	var err error
 
 	if out.Column, err = DecodeColumn[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Column: %w", err)
 	}
 	if out.InclusionProof, err = DecodeMerkleProof[H](r); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode InclusionProof: %w", err)
 	}
 
 	return out, nil
@@ -300,20 +298,21 @@ func DecodeColumn[H HasherDomain](r io.Reader) (Column[H], error) {
 	var err error
 
 	if err = binary.Read(r, binary.LittleEndian, &out.Index); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Index: %w", err)
 	}
 
-	numRows, err := binary.ReadUvarint(r.(io.ByteReader))
-	if err != nil {
-		return out, err
+	var rowsLength uint64
+	if err = binary.Read(r, binary.LittleEndian, &rowsLength); err != nil {
+		return out, fmt.Errorf("failed to read Rows length: %w", err)
 	}
-	out.Rows = make([]H, numRows)
-	for i := uint64(0); i < numRows; i++ {
-		out.Rows[i], err = DecodeHasherDomain[H](r)
-		if err != nil {
-			return out, err
+	out.Rows = make([]H, rowsLength)
+	for i := uint64(0); i < rowsLength; i++ {
+		if out.Rows[i], err = DecodeHasherDomain[H](r); err != nil {
+			return out, fmt.Errorf("failed to decode Row: %w", err)
 		}
 	}
+
+	// Note: We're ignoring the _h field as it's not used in Go
 
 	return out, nil
 }
@@ -322,23 +321,23 @@ func DecodeLabelingProof[H HasherDomain](r io.Reader) (LabelingProof[H], error) 
 	var out LabelingProof[H]
 	var err error
 
-	numParents, err := binary.ReadUvarint(r.(io.ByteReader))
-	if err != nil {
-		return out, err
+	var parentsLength uint64
+	if err = binary.Read(r, binary.LittleEndian, &parentsLength); err != nil {
+		return out, fmt.Errorf("failed to read Parents length: %w", err)
 	}
-	out.Parents = make([]H, numParents)
-	for i := uint64(0); i < numParents; i++ {
-		out.Parents[i], err = DecodeHasherDomain[H](r)
-		if err != nil {
-			return out, err
+	out.Parents = make([]H, parentsLength)
+	for i := uint64(0); i < parentsLength; i++ {
+		if out.Parents[i], err = DecodeHasherDomain[H](r); err != nil {
+			return out, fmt.Errorf("failed to decode Parent: %w", err)
 		}
 	}
 
 	if err = binary.Read(r, binary.LittleEndian, &out.LayerIndex); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode LayerIndex: %w", err)
 	}
+
 	if err = binary.Read(r, binary.LittleEndian, &out.Node); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Node: %w", err)
 	}
 
 	return out, nil
@@ -348,57 +347,48 @@ func DecodeEncodingProof[H HasherDomain](r io.Reader) (EncodingProof[H], error) 
 	var out EncodingProof[H]
 	var err error
 
-	numParents, err := binary.ReadUvarint(r.(io.ByteReader))
-	if err != nil {
-		return out, err
+	var parentsLength uint64
+	if err = binary.Read(r, binary.LittleEndian, &parentsLength); err != nil {
+		return out, fmt.Errorf("failed to read Parents length: %w", err)
 	}
-	out.Parents = make([]H, numParents)
-	for i := uint64(0); i < numParents; i++ {
-		out.Parents[i], err = DecodeHasherDomain[H](r)
-		if err != nil {
-			return out, err
+	out.Parents = make([]H, parentsLength)
+	for i := uint64(0); i < parentsLength; i++ {
+		if out.Parents[i], err = DecodeHasherDomain[H](r); err != nil {
+			return out, fmt.Errorf("failed to decode Parent: %w", err)
 		}
 	}
 
 	if err = binary.Read(r, binary.LittleEndian, &out.LayerIndex); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode LayerIndex: %w", err)
 	}
+
 	if err = binary.Read(r, binary.LittleEndian, &out.Node); err != nil {
-		return out, err
+		return out, fmt.Errorf("failed to decode Node: %w", err)
 	}
 
 	return out, nil
 }
 
-func DecodeCommitment(r io.Reader) (Commitment, error) {
-	var out Commitment
-	_, err := io.ReadFull(r, out[:])
-	return out, err
+func DecodeHasherDomain[H HasherDomain](r io.Reader) (H, error) {
+	var out H
+	if err := binary.Read(r, binary.LittleEndian, &out); err != nil {
+		return out, fmt.Errorf("failed to decode HasherDomain: %w", err)
+	}
+	return out, nil
 }
 
 func DecodeTicket(r io.Reader) (Ticket, error) {
 	var out Ticket
-	_, err := io.ReadFull(r, out[:])
-	return out, err
+	if _, err := io.ReadFull(r, out[:]); err != nil {
+		return out, fmt.Errorf("failed to decode Ticket: %w", err)
+	}
+	return out, nil
 }
 
-func DecodeStringRegisteredProofType(r io.Reader) (StringRegisteredProofType, error) {
-	length, err := binary.ReadUvarint(r.(io.ByteReader))
-	if err != nil {
-		return "", err
+func DecodeCommitment(r io.Reader) (Commitment, error) {
+	var out Commitment
+	if _, err := io.ReadFull(r, out[:]); err != nil {
+		return out, fmt.Errorf("failed to decode Commitment: %w", err)
 	}
-
-	bytes := make([]byte, length)
-	_, err = io.ReadFull(r, bytes)
-	if err != nil {
-		return "", err
-	}
-
-	return StringRegisteredProofType(bytes), nil
-}
-
-func DecodeHasherDomain[H HasherDomain](r io.Reader) (H, error) {
-	var out H
-	err := binary.Read(r, binary.LittleEndian, &out)
-	return out, err
+	return out, nil
 }
