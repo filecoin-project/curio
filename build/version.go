b9559e7c3b4fdf580b5ff2463dc96dd3837ef724
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2024-07-16 00:44:11 +0400

    release v1.21.1 (#94)

--- build/version.go
+++ build/version.go
@@ -41,7 +41,7 @@ func BuildTypeString() string {
 }
 
 // Intent: Major.Network.EvenReleases
-var BuildVersionArray = [3]int{1, 22, 0}
+var BuildVersionArray = [3]int{1, 22, 1}
 
 // Ex: "1.2.3"
 var BuildVersion = strings.Join(lo.Map(BuildVersionArray[:],
--- documentation/en/curio-cli/sptool.md
+++ documentation/en/curio-cli/sptool.md
@@ -7,7 +7,7 @@ USAGE:
    sptool [global options] command [command options] [arguments...]
 
 VERSION:
-   1.22.0
+   1.22.1
 
 COMMANDS:
    actor    Manage Filecoin Miner Actor Metadata
