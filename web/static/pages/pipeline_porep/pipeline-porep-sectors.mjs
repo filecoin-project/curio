1bfd69b6d48bc1c6c90b98aa9974affe51dc3d2b
LexLuthr <lexluthr@curiostorage.org>
2024-07-25 23:02:31 +0400

    fix porep ui

--- web/static/pages/pipeline_porep/pipeline-porep-sectors.mjs
+++ web/static/pages/pipeline_porep/pipeline-porep-sectors.mjs
@@ -8,45 +8,10 @@ customElements.define('pipeline-porep-sectors',class PipelinePorepSectors extend
     }
     async loadData() {
         this.data = await RPCCall('PipelinePorepSectors');
+        setTimeout(() => this.loadData(), 3000);
         this.requestUpdate();
-
-        // Poll for updates
-        setInterval(async () => {
-            this.data = await RPCCall('ActorSummary');
-            this.requestUpdate();
-        }, 30000);
     };
 
-    render() {
-        return html`
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-            <link rel="stylesheet" href="/ux/main.css" onload="document.body.style.visibility = 'initial'">
-            <style>${PipelinePorepSectors.styles}</style>
-            <table class="table table-dark porep-state porep-pipeline-table">
-                <tbody>
-                    ${this.renderSectors()}
-                </tbody>
-            </table>
-        `;
-    }
-
-    renderSectors() {
-        return this.data.map((sector) => html`
-            <tr>
-                <td>${sector.Address}</td>
-                <td rowspan="2">${sector.CreateTime}</td>
-                <td rowspan="2"><sector-porep-state .sector=${sector}></sector-porep-state></td>
-                <td rowspan="2">
-                    <a href="/pages/sector/?sp=${sector.Address}&id=${sector.SectorNumber}">DETAILS</a>
-                </td>
-            </tr>
-            <tr>
-                <td>${sector.SectorNumber}</td>
-            </tr>
-        `);
-    }
-} );
-customElements.define('sector-porep-state', class SectorPorepState extends LitElement {
     static styles = css`
         .porep-pipeline-table,
         .porep-state {
@@ -57,11 +22,11 @@ customElements.define('sector-porep-state', class SectorPorepState extends LitEl
         .porep-pipeline-table th {
             border-left: none;
             border-collapse: collapse;
+            vertical-align: middle;
         }
 
         .porep-pipeline-table tr:nth-child(odd) {
             border-top: 6px solid #999999;
-
         }
 
         .porep-pipeline-table tr:first-child,
@@ -107,7 +72,35 @@ customElements.define('sector-porep-state', class SectorPorepState extends LitEl
     }
 
     render() {
-        const sector = this.sector;
+        return html`
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <link rel="stylesheet" href="/ux/main.css" onload="document.body.style.visibility = 'initial'">
+            <style>${PipelinePorepSectors.styles}</style>
+            <table class="table table-dark porep-state porep-pipeline-table">
+                <tbody>
+                    ${this.renderSectors()}
+                </tbody>
+            </table>
+        `;
+    }
+
+    renderSectors() {
+        return this.data.map((sector) => html`
+            <tr>
+                <td>${sector.Address}</td>
+                <td rowspan="2">${sector.CreateTime}</td>
+                <td rowspan="2">${this.renderSector(sector)}</td>
+                <td rowspan="2">
+                    <a href="/pages/sector/?sp=${sector.Address}&id=${sector.SectorNumber}">DETAILS</a>
+                </td>
+            </tr>
+            <tr>
+                <td>${sector.SectorNumber}</td>
+            </tr>
+        `);
+    }
+
+    renderSector(sector) {
         return html`
             <table class="porep-state">
                 <tbody>
@@ -163,4 +156,5 @@ customElements.define('sector-porep-state', class SectorPorepState extends LitEl
             </td>
         `;
     }
-});
+
+} );
