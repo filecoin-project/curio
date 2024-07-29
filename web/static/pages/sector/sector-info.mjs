4e1b16f92afb72047711b884efb8fc9b2df5bebf
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2024-07-30 00:30:57 +0400

    fix sector detail page (#128)

--- web/static/pages/sector/sector-info.mjs
+++ web/static/pages/sector/sector-info.mjs
@@ -101,10 +101,12 @@ customElements.define('sector-info',class SectorInfo extends LitElement {
                         <th>Host</th>
                     </tr>
                     ${this.data.Locations.map(location => html`
-                        ${location.PathType ? html`<td rowspan="${location.PathTypeRowSpan}">${location.PathType}</td>` : ''}
-                        ${location.FileType ? html`<td rowspan="${location.FileTypeRowSpan}">${location.FileType}</td>` : ''}
-                        <td>${location.Locations[0].StorageID}</td>
-                        <td>${location.Locations[0].Urls.map(url => html`<p>${url}</p>`)}</td>
+                        <tr>
+                            ${location.PathType ? html`<td rowspan="${location.PathTypeRowSpan}">${location.PathType}</td>` : ''}
+                            ${location.FileType ? html`<td rowspan="${location.FileTypeRowSpan}">${location.FileType}</td>` : ''}
+                            <td>${location.Locations[0].StorageID}</td>
+                            <td>${location.Locations[0].Urls.map(url => html`<p>${url}</p>`)}</td>
+                        </tr>
                         ${location.Locations.slice(1).map(loc => html`
                             <tr>
                                 <td>${loc.StorageID}</td>
@@ -116,7 +118,7 @@ customElements.define('sector-info',class SectorInfo extends LitElement {
             </div>
             <div>
                 <h3>Tasks</h3>
-                <table class="porep-state">
+                <table class="table table-dark">
                     <tr>
                         <th>Task Type</th>
                         <th>Task ID</th>
