8f8daa350b2820e4737cdb088ae85f8e7146026b
Andrew Jackson (Ajax) <snadrus@gmail.com>
2024-07-30 16:31:07 -0500

    json tracer

--- go.mod
+++ go.mod
@@ -21,7 +21,7 @@ require (
 	github.com/filecoin-project/go-commp-utils v0.1.3
 	github.com/filecoin-project/go-commp-utils/nonffi v0.0.0-20220905160352-62059082a837
 	github.com/filecoin-project/go-fil-commcid v0.1.0
-	github.com/filecoin-project/go-jsonrpc v0.5.0
+	github.com/filecoin-project/go-jsonrpc v0.5.1-0.20240730181539-2bb4fbb48fdd
 	github.com/filecoin-project/go-padreader v0.0.1
 	github.com/filecoin-project/go-state-types v0.14.0-rc6
 	github.com/filecoin-project/go-statestore v0.2.0
--- web/api/webrpc/routes.go
+++ web/api/webrpc/routes.go
@@ -43,13 +43,13 @@ func Routes(r *mux.Router, deps *deps.Deps, debug bool) {
 
 	opt := []jsonrpc.ServerOption{}
 	if debug {
-		opt = append(opt, jsonrpc.WithLogger(func(method string, params []reflect.Value, results []reflect.Value, err error) {
+		opt = append(opt, jsonrpc.WithTracer(func(method string, params []reflect.Value, results []reflect.Value, err error) {
 			resNil := len(results) == 0 || (len(results) == 1 && results[0].IsNil())
 			log.Infow("WebRPC call", "method", method, "params", params, "resultsNil?", resNil, "error", err)
 		}))
 
 	}
-	rpcSrv := jsonrpc.NewServer(opt)
+	rpcSrv := jsonrpc.NewServer(opt...)
 	rpcSrv.Register("CurioWeb", handler)
 	r.Handle("/v0", rpcSrv)
 }
