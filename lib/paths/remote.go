65e1e6049f19bcbbfd449470bd7773a41c17657f
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2024-08-09 12:33:30 +0400

    fix: readRemote range request input (#136)
    * fix readRemote
    
    * use 0 isntead of -1
    
    * remove bad line
    
    * fix gen
    
    * only set range when needed
    
    ---------
    
    Co-authored-by: Łukasz Magiera <magik6k@gmail.com>

--- lib/paths/remote.go
+++ lib/paths/remote.go
@@ -487,7 +487,15 @@ func (r *Remote) readRemote(ctx context.Context, url string, offset, size abi.Pa
 	if r.auth != nil {
 		req.Header = r.auth.Clone()
 	}
-	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", offset, offset+size-1))
+
+	if offset != 0 || size != 0 {
+		req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", offset, offset+size-1))
+
+		if size == 0 {
+			req.Header.Set("Range", fmt.Sprintf("bytes=%d-", offset))
+		}
+	}
+
 	req = req.WithContext(ctx)
 
 	resp, err := http.DefaultClient.Do(req)
