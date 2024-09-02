8617ce7d417f450b875e21a010478038b3f6bc21
Łukasz Magiera <magik6k@gmail.com>
2024-09-02 18:58:09 +0200

    What

--- lib/ffi/cunative/decode_snap.go
+++ lib/ffi/cunative/decode_snap.go
@@ -17,7 +17,7 @@ package cunative
 //         blst_fr_from_lendian(&key_fr, key + i * node_size);
 //
 //         // Read rho inverse
-//         blst_fr_from_lendian(&rho_inv_fr, rho_invs + i * 32);  // Assuming rho_invs are 32 bytes each
+//         blst_fr_from_lendian(&rho_inv_fr, rho_invs + i * 32);
 //
 //         // Perform the decoding operation
 //         blst_fr_sub(&out_fr, &replica_fr, &key_fr);
@@ -30,7 +30,7 @@ package cunative
 import "C"
 
 import (
-	"bufio"
+	"encoding/binary"
 	"github.com/filecoin-project/curio/lib/proof"
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -41,161 +41,11 @@ import (
 	"io"
 	"math/big"
 	"math/bits"
+	"unsafe"
 )
 
-/*
-
-   let comm_r_old = <TreeRHasher as Hasher>::Function::hash2(&comm_c, &comm_sector_key);
-        let phi = phi(&comm_d_new, &comm_r_old);
-        let chunk_size: usize = std::cmp::min(base_tree_nodes_count, CHUNK_SIZE_MIN);
-
-        let rho_invs = Rhos::new_inv(&phi, h, nodes_count);
-
-
-
-Reference:
-func CommR(commC, commRLast [32]byte) ([32]byte, error) {
-	// reverse commC and commRLast so that endianness is correct
-	for i, j := 0, len(commC)-1; i < j; i, j = i+1, j-1 {
-		commC[i], commC[j] = commC[j], commC[i]
-		commRLast[i], commRLast[j] = commRLast[j], commRLast[i]
-	}
-
-	input_a := new(big.Int)
-	input_a.SetBytes(commC[:])
-	input_b := new(big.Int)
-	input_b.SetBytes(commRLast[:])
-	input := []*big.Int{input_a, input_b}
-
-	cons, err := poseidon.GenPoseidonConstants(3)
-	if err != nil {
-		return [32]byte{}, err
-	}
-
-	h1, err := poseidon.Hash(input, cons, poseidon.OptimizedStatic)
-	if err != nil {
-		return [32]byte{}, err
-	}
-
-	h1element := new(ff.Element).SetBigInt(h1).Bytes()
-
-	// reverse the bytes so that endianness is correct
-	for i, j := 0, len(h1element)-1; i < j; i, j = i+1, j-1 {
-		h1element[i], h1element[j] = h1element[j], h1element[i]
-	}
-
-	return h1element, nil
-}
-
-impl HashFunction<PoseidonDomain> for PoseidonFunction {
-    fn hash(data: &[u8]) -> PoseidonDomain {
-        shared_hash(data)
-    }
-
-    fn hash2(a: &PoseidonDomain, b: &PoseidonDomain) -> PoseidonDomain {
-        let mut p =
-            Poseidon::new_with_preimage(&[(*a).into(), (*b).into()][..], &*POSEIDON_CONSTANTS_2);
-        let fr: Fr = p.hash();
-        fr.into()
-    }
-...
-        let comm_r_old = <TreeRHasher as Hasher>::Function::hash2(&comm_c, &comm_sector_key);
-
-...
-
-
-    /// Creates [`Poseidon`] instance using provided preimage and [`PoseidonConstants`] as input.
-    /// Doesn't support [`PoseidonConstants`] with [`HashType::VariableLength`]. It is assumed that
-    /// size of input preimage set can't be greater than [`Arity`].
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use neptune::poseidon::PoseidonConstants;
-    /// use neptune::poseidon::Poseidon;
-    /// use pasta_curves::Fp;
-    /// use ff::Field;
-    /// use generic_array::typenum::U2;
-    ///
-    /// let preimage_set_length = 1;
-    /// let constants: PoseidonConstants<Fp, U2> = PoseidonConstants::new_constant_length(preimage_set_length);
-    ///
-    /// let preimage = vec![Fp::from(u64::MAX); preimage_set_length];
-    ///
-    /// let mut poseidon = Poseidon::<Fp, U2>::new_with_preimage(&preimage, &constants);
-    ///
-    /// assert_eq!(constants.width(), 3);
-    /// assert_eq!(poseidon.elements.len(), constants.width());
-    /// assert_eq!(poseidon.elements[1], Fp::from(u64::MAX));
-    /// assert_eq!(poseidon.elements[2], Fp::ZERO);
-    /// ```
-    pub fn new_with_preimage(preimage: &[F], constants: &'a PoseidonConstants<F, A>) -> Self {
-        let elements = match constants.hash_type {
-            HashType::ConstantLength(constant_len) => {
-                assert_eq!(constant_len, preimage.len(), "Invalid preimage size");
-
-                GenericArray::generate(|i| {
-                    if i == 0 {
-                        constants.domain_tag
-                    } else if i > preimage.len() {
-                        F::ZERO
-                    } else {
-                        preimage[i - 1]
-                    }
-                })
-            }
-            HashType::MerkleTreeSparse(_) => {
-                panic!("Merkle Tree (with some empty leaves) hashes are not yet supported.")
-            }
-            HashType::VariableLength => panic!("variable-length hashes are not yet supported."),
-            _ => {
-                assert_eq!(preimage.len(), A::to_usize(), "Invalid preimage size");
-
-                GenericArray::generate(|i| {
-                    if i == 0 {
-                        constants.domain_tag
-                    } else {
-                        preimage[i - 1]
-                    }
-                })
-            }
-        };
-        let width = preimage.len() + 1;
-
-        Poseidon {
-            constants_offset: 0,
-            current_round: 0,
-            elements,
-            pos: width,
-            constants,
-            _f: PhantomData::<F>,
-        }
-    }
-
-// Use a custom domain separation tag when generating randomness phi, rho, and challenges bits.
-pub const HASH_TYPE_GEN_RANDOMNESS: HashType<Fr, U2> = HashType::Custom(CType::Arbitrary(1));
-
-lazy_static! {
-    pub static ref POSEIDON_CONSTANTS_GEN_RANDOMNESS: PoseidonConstants::<Fr, U2> =
-        PoseidonConstants::new_with_strength_and_type(Strength::Standard, HASH_TYPE_GEN_RANDOMNESS);
-}
-
-
-// `phi = H(comm_d_new, comm_r_old)` where Poseidon uses the custom "gen randomness" domain
-// separation tag.
-#[inline]
-pub fn phi<TreeDDomain: Domain>(comm_d_new: &TreeDDomain, comm_r_old: &TreeRDomain) -> TreeRDomain {
-    let comm_d_new: Fr = (*comm_d_new).into();
-    let comm_r_old: Fr = (*comm_r_old).into();
-    Poseidon::new_with_preimage(
-        &[comm_d_new, comm_r_old],
-        &POSEIDON_CONSTANTS_GEN_RANDOMNESS,
-    )
-    .hash()
-    .into()
-}
-
-*/
+type B32le = [32]byte
+type BytesLE = []byte
 
 func DecodeSnap(spt abi.RegisteredSealProof, commD, commK cid.Cid, key, replica io.Reader, out io.Writer) error {
 	ssize, err := spt.SectorSize()
@@ -228,57 +78,42 @@ func DecodeSnap(spt abi.RegisteredSealProof, commD, commK cid.Cid, key, replica 
 		return xerrors.Errorf("failed to compute rho inverses: %w", err)
 	}
 
-	// Setup buffered readers and writers
-	keyReader := bufio.NewReader(key)
-	replicaReader := bufio.NewReader(replica)
-	outWriter := bufio.NewWriter(out)
+	// Allocate buffers
+	replicaBuffer := make([]byte, ssize)
+	keyBuffer := make([]byte, ssize)
+	outBuffer := make([]byte, ssize)
 
-	// Process data in chunks
-	const dataBlockSize = 1 << 10
-	buffer := make([]byte, dataBlockSize)
-	for chunkIndex := uint64(0); ; chunkIndex += dataBlockSize {
-		n, err := io.ReadFull(replicaReader, buffer)
-		if err == io.EOF {
-			break
-		}
-		if err != nil && err != io.ErrUnexpectedEOF {
-			return xerrors.Errorf("failed to read replica data: %w", err)
-		}
-
-		for i := uint64(0); i < uint64(n); i += proof.NODE_SIZE {
-			inputIndex := chunkIndex + i
-			nodeIndex := inputIndex / proof.NODE_SIZE
-
-			// Read sector key data
-			sectorKeyData := make([]byte, proof.NODE_SIZE)
-			_, err := io.ReadFull(keyReader, sectorKeyData)
-			if err != nil {
-				return xerrors.Errorf("failed to read sector key data: %w", err)
-			}
-
-			// Convert bytes to field elements
-			sectorKeyFr := bytesToFr(sectorKeyData)
-			replicaDataFr := bytesToFr(buffer[i : i+proof.NODE_SIZE])
-
-			// Get rho inverse
-			rhoInv := rhoInvs.Get(nodeIndex)
-
-			// Perform the decoding operation
-			outDataFr := frSub(replicaDataFr, sectorKeyFr)
-			outDataFr = frMul(outDataFr, rhoInv)
-
-			// Convert back to bytes and write
-			outData := frToBytes(outDataFr)
-			_, err = outWriter.Write(outData)
-			if err != nil {
-				return xerrors.Errorf("failed to write output data: %w", err)
-			}
-		}
+	// Read all data into buffers
+	_, err = io.ReadFull(replica, replicaBuffer)
+	if err != nil {
+		return xerrors.Errorf("failed to read replica data: %w", err)
+	}
+	_, err = io.ReadFull(key, keyBuffer)
+	if err != nil {
+		return xerrors.Errorf("failed to read key data: %w", err)
 	}
 
-	err = outWriter.Flush()
+	// Convert rhoInvs to byte slice
+	rhoInvsBytes := make([]byte, nodesCount*32)
+	for i := uint64(0); i < nodesCount; i++ {
+		rhoInv := rhoInvs.Get(i)
+		copy(rhoInvsBytes[i*32:(i+1)*32], rhoInv[:])
+	}
+
+	// Call the C function
+	C.snap_decode_loop(
+		(*C.uint8_t)(unsafe.Pointer(&replicaBuffer[0])),
+		(*C.uint8_t)(unsafe.Pointer(&keyBuffer[0])),
+		(*C.uint8_t)(unsafe.Pointer(&rhoInvsBytes[0])),
+		(*C.uint8_t)(unsafe.Pointer(&outBuffer[0])),
+		C.size_t(nodesCount),
+		C.size_t(proof.NODE_SIZE),
+	)
+
+	// Write the result
+	_, err = out.Write(outBuffer)
 	if err != nil {
-		return xerrors.Errorf("failed to flush output: %w", err)
+		return xerrors.Errorf("failed to write output data: %w", err)
 	}
 
 	return nil
@@ -286,15 +121,9 @@ func DecodeSnap(spt abi.RegisteredSealProof, commD, commK cid.Cid, key, replica 
 
 // Phi implements the phi function as described in the Rust code.
 // It computes phi = H(comm_d_new, comm_r_old) using Poseidon hash with a custom domain separation tag.
-func Phi(commDNew, commROld []byte) ([32]byte, error) {
-	// Reverse commDNew and commROld for correct endianness
-	for i, j := 0, len(commDNew)-1; i < j; i, j = i+1, j-1 {
-		commDNew[i], commDNew[j] = commDNew[j], commDNew[i]
-		commROld[i], commROld[j] = commROld[j], commROld[i]
-	}
-
-	inputA := new(big.Int).SetBytes(commDNew[:])
-	inputB := new(big.Int).SetBytes(commROld[:])
+func Phi(commDNew, commROld BytesLE) ([32]byte, error) {
+	inputA := bigIntLE(commDNew)
+	inputB := bigIntLE(commROld)
 	input := []*big.Int{inputA, inputB}
 
 	// Generate Poseidon constants with a custom domain separation tag
@@ -309,12 +138,7 @@ func Phi(commDNew, commROld []byte) ([32]byte, error) {
 		return [32]byte{}, xerrors.Errorf("failed to compute Poseidon hash: %w", err)
 	}
 
-	hElement := new(ff.Element).SetBigInt(h).Bytes()
-
-	// Reverse the bytes for correct endianness
-	for i, j := 0, len(hElement)-1; i < j; i, j = i+1, j-1 {
-		hElement[i], hElement[j] = hElement[j], hElement[i]
-	}
+	hElement := ffElementBytesLE(new(ff.Element).SetBigInt(h))
 
 	return hElement, nil
 }
@@ -346,7 +170,7 @@ func rho(phi [32]byte, high uint32) (*ff.Element, error) {
 
 // Rhos represents a collection of precomputed rho values
 type Rhos struct {
-	rhos    map[uint64]ff.Element
+	rhos    map[uint64]B32le
 	bitsShr uint64
 }
 
@@ -360,7 +184,7 @@ func NewInvRange(phi [32]byte, h uint64, nodesCount, offset, num uint64) (*Rhos,
 	bitsShr := calcBitsShr(h, nodesCount)
 	highRange := calcHighRange(offset, num, bitsShr)
 
-	rhos := make(map[uint64]ff.Element)
+	rhos := make(map[uint64]B32le)
 	for high := highRange.Start; high <= highRange.End; high++ {
 		rhoVal, err := rho(phi, uint32(high))
 		if err != nil {
@@ -368,7 +192,7 @@ func NewInvRange(phi [32]byte, h uint64, nodesCount, offset, num uint64) (*Rhos,
 		}
 
 		invRho := new(ff.Element).Inverse(rhoVal) // same as blst_fr_eucl_inverse??
-		rhos[high] = *invRho
+		rhos[high] = ffElementBytesLE(invRho)
 	}
 
 	return &Rhos{
@@ -378,7 +202,7 @@ func NewInvRange(phi [32]byte, h uint64, nodesCount, offset, num uint64) (*Rhos,
 }
 
 // Get retrieves the rho for a specific node offset
-func (r *Rhos) Get(offset uint64) ff.Element {
+func (r *Rhos) Get(offset uint64) B32le {
 	high := offset >> r.bitsShr
 	return r.rhos[high]
 }
@@ -427,4 +251,24 @@ func hDefault(nodesCount uint64) uint64 {
 		return 1
 	}
 	return 10
+}
+
+func ffElementBytesLE(z *ff.Element) (res B32le) {
+	_z := z.ToRegular()
+	binary.LittleEndian.PutUint64(res[0:8], _z[0])
+	binary.LittleEndian.PutUint64(res[8:16], _z[1])
+	binary.LittleEndian.PutUint64(res[16:24], _z[2])
+	binary.LittleEndian.PutUint64(res[24:32], _z[3])
+
+	return
+}
+
+// BE new(big.Int).SetBytes(commDNew[:])
+func bigIntLE(b []byte) *big.Int {
+	// invert b
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+
+	return new(big.Int).SetBytes(b)
 }
