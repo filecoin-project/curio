6122c8f1aa0f224f6bc9916ed0588f5db94261f9
Ruben Gómez Morales <rubengomez1000@gmail.com>
2024-09-23 14:02:09 +0200

    Update recover_task.go

--- tasks/window/recover_task.go
+++ tasks/window/recover_task.go
@@ -267,7 +267,7 @@ func (w *WdPostRecoverDeclareTask) processHeadChange(ctx context.Context, revert
 		}
 
 		// repeat checks for [2, 4, 6, 8, 10, 12], the closer deadline is prioritized
-		for i := 0; i < 7; i++ {
+		for i := 1; i < 7; i++ {
 			// declaring 2*i deadlines ahead
 			declDeadline := (di.Index + uint64(2*i)) % di.WPoStPeriodDeadlines
 
