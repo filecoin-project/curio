eb34903a3e358d932bfae98a2088aae057c713f5
Łukasz Magiera <magik6k@gmail.com>
2024-10-04 17:38:17 +0200

    slowly getting data import to actually work

--- pdp/auth.go
+++ pdp/auth.go
@@ -11,25 +11,25 @@ import (
 )
 
 // verifyJWTToken extracts and verifies the JWT token from the request and returns the serviceID.
-func (p *PDPService) verifyJWTToken(r *http.Request) (int64, error) {
+func (p *PDPService) verifyJWTToken(r *http.Request) (string, error) {
 	// Get the Authorization header
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" {
-		return 0, fmt.Errorf("missing Authorization header")
+		return "", fmt.Errorf("missing Authorization header")
 	}
 
 	// Extract the token from the header
 	bearerTokenPrefix := "Bearer "
 	if !strings.HasPrefix(authHeader, bearerTokenPrefix) {
-		return 0, fmt.Errorf("invalid Authorization header format")
+		return "", fmt.Errorf("invalid Authorization header format")
 	}
 	tokenString := strings.TrimSpace(strings.TrimPrefix(authHeader, bearerTokenPrefix))
 	if tokenString == "" {
-		return 0, fmt.Errorf("empty token")
+		return "", fmt.Errorf("empty token")
 	}
 
 	// Variable to capture serviceID
-	var serviceID int64
+	var service string
 
 	// Parse and verify the JWT token
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
@@ -45,9 +45,14 @@ func (p *PDPService) verifyJWTToken(r *http.Request) (int64, error) {
 		}
 
 		// Extract service_id from claims
-		serviceID, ok := claims["service_name"]
+		serviceIf, ok := claims["service_name"]
 		if !ok {
 			return nil, fmt.Errorf("missing service_name claim")
+		}
+
+		service, ok = serviceIf.(string)
+		if !ok {
+			return nil, fmt.Errorf("invalid service_name claim")
 		}
 
 		// Query the database for the public key using serviceID
@@ -55,9 +60,9 @@ func (p *PDPService) verifyJWTToken(r *http.Request) (int64, error) {
 		ctx := r.Context()
 		err := p.db.QueryRow(ctx, `
             SELECT pubkey FROM pdp_services WHERE service_label=$1
-        `, serviceID).Scan(&pubKeyBytes)
+        `, service).Scan(&pubKeyBytes)
 		if err != nil {
-			return nil, fmt.Errorf("failed to retrieve public key for service_id %d: %v", serviceID, err)
+			return nil, fmt.Errorf("failed to retrieve public key for service_id %s: %v", service, err)
 		}
 
 		// Parse the public key
@@ -74,11 +79,11 @@ func (p *PDPService) verifyJWTToken(r *http.Request) (int64, error) {
 		return pubKey, nil
 	})
 	if err != nil {
-		return 0, err
+		return "", err
 	}
 	if !token.Valid {
-		return 0, fmt.Errorf("invalid token")
+		return "", fmt.Errorf("invalid token")
 	}
 
-	return serviceID, nil
+	return service, nil
 }
