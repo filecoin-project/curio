28194c195e53aa5647ff43e55c8f7acb4b828ca7
Łukasz Magiera <magik6k@gmail.com>
2024-10-07 18:50:24 +0200

    wire up proofset create

--- cmd/curio/tasks/tasks.go
+++ cmd/curio/tasks/tasks.go
@@ -261,8 +261,11 @@ func StartTasks(ctx context.Context, dependencies *deps.Deps) (*harmonytask.Task
 			}
 		}
 
+		var sdeps cuhttp.ServiceDeps
+
 		if cfg.Subsystems.EnablePDP {
-			_ = getSenderEth
+			es := getSenderEth()
+			sdeps.EthSender = es
 
 			pdpNotifTask := pdp.NewPDPNotifyTask(db)
 			activeTasks = append(activeTasks, pdpNotifTask)
@@ -273,7 +276,7 @@ func StartTasks(ctx context.Context, dependencies *deps.Deps) (*harmonytask.Task
 		activeTasks = append(activeTasks, indexingTask, ipniTask)
 
 		if cfg.HTTP.Enable {
-			err = cuhttp.StartHTTPServer(ctx, dependencies)
+			err = cuhttp.StartHTTPServer(ctx, dependencies, &sdeps)
 			if err != nil {
 				return nil, xerrors.Errorf("failed to start the HTTP server: %w", err)
 			}
