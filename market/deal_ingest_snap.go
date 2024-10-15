be462de252c927e77608719ac247167dd5ea0e04
Łukasz Magiera <magik6k@gmail.com>
2024-10-16 00:23:44 +0200

    fix: snap: Correctly handle non-filplus deals

--- market/deal_ingest_snap.go
+++ market/deal_ingest_snap.go
@@ -30,6 +30,7 @@ import (
 )
 
 const IdealEndEpochBuffer = 2 * builtin.EpochsInDay
+const MaxEndEpochBufferUnverified = 180 * builtin.EpochsInDay
 
 // assuming that snap takes up to 20min to get to submitting the message we want to avoid sectors from deadlines which will
 // become immutable in the next 20min (40 epochs)
@@ -264,6 +265,8 @@ func (p *PieceIngesterSnap) AllocatePieceToSector(ctx context.Context, maddr add
 		vd.tmax = alloc.TermMax
 
 		maxExpiration = int64(head.Height() + alloc.TermMax)
+	} else {
+		maxExpiration = int64(piece.DealSchedule.EndEpoch) + MaxEndEpochBufferUnverified
 	}
 	propJson, err = json.Marshal(piece.PieceActivationManifest)
 	if err != nil {
