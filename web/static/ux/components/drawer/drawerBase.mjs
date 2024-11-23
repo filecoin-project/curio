0ae3d441f2d18c23d847e4ada7496f3c88900ac7
A.Hoffa <60316298+anhoffa@users.noreply.github.com>
2024-11-23 14:35:32 +0100

    Update the drawer

--- web/static/ux/components/drawer/drawerBase.mjs
+++ web/static/ux/components/drawer/drawerBase.mjs
@@ -21,7 +21,7 @@ class DrawerBase extends StyledLitElement {
       const dialog = this.shadowRoot.querySelector('dialog');
 
       if (this.isOpen) {
-        dialog.showModal();
+        dialog.show();
       } else {
         dialog.close();
       }
@@ -43,20 +43,25 @@ class DrawerBase extends StyledLitElement {
 
   render() {
     return html`
-        <div class="dialog-wrapper">
-<!--            <button class="btn" ><<</button>-->
-            <ui-drawer-button targetSelector="#ui-dialog" @click=${this.handleToggle}></ui-drawer-button>
-            <dialog
-                    id="ui-dialog"
-                    class="dialog"
-                    aria-label=${this.label}
-                    @close=${this.handleClose}
-                    @cancel=${this.handleClose}
-            >
-                <slot name="close-button"></slot>
-                <slot></slot>
-            </dialog>
-        </div>
+      <div>
+        <button class="open-btn" @click=${this.handleToggle}>Cluster Tasks</button>
+        <dialog
+          class="dialog"
+          aria-label=${this.label}
+          @close=${this.handleClose}
+          @cancel=${this.handleClose}
+        >
+          <div class="dialog-header">
+            <slot name="header"></slot>
+            <button class="close-btn" @click=${this.handleClose}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg>
+            </button>
+          </div>
+          <slot name="content"></slot>
+        </dialog>
+      </div>
     `;
   }
 }
@@ -64,41 +69,63 @@ class DrawerBase extends StyledLitElement {
 // todo: fix the jumping width issue
 DrawerBase.styles = [
   css`
-    .dialog-wrapper {
-      //position: relative;
-      //display: flex;
-      border: 2px solid #1DC8CC;
-    }
-    
-    .btn {
-      position: relative;
-      right: 5rem;
-      background-color: red;
-      z-index: 10000;
-    }
-    
     dialog {
       position: fixed;
       left: auto;
       right: 0;
       top: 0;
-      width: max-content;
+      width: 40rem; // todo
       min-height: 100vh;
       padding: 1rem;
       border: 0;
-      background-color: #28282a; /* todo */
+      background-color: #2e2e33;
       color: var(--color-text-primary);
-      box-shadow: -10px 0 20px 4px rgba(84, 59, 147, 0.8);
+      box-shadow: -8px 0 20px 4px #242428;
     }
 
-    dialog::backdrop {
-      //background: rgba(0, 0, 0, 0.5);
+    .dialog-header {
+      display: flex;
+    }
+    
+    *::slotted([name="header"]) {
+      border: 2px solid red;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
     }
 
-    ::slotted([slot="close-button"]) {
+    ::slotted([name="content"]) {
+      border: 2px solid red;
+      overflow: scroll;
+    }
+    
+    .open-btn {
       position: absolute;
-      top: 1rem;
-      right: 1rem;
+      right: 0;
+      transform-origin: bottom right;
+      transform: rotate(-90deg);
+      color: var(--color-text-primary);
+      background-color: var(--color-secondary-light);
+      border-radius: 8px 8px 0 0;
+      padding: 0.75rem 1.2rem;
+
+      &:hover, &:active {
+        background-color: var(--color-secondary-main);
+      }
+    }
+    
+    .close-btn {
+      all: unset;
+      position: absolute;
+      top: 2rem;
+      right: 2rem;
+      background: transparent;
+      color: var(--color-text-primary);
+
+      &:hover, &:active {
+        color: var(--color-text-primary);
+        opacity: 0.8;
+      }
     }
   `
 ]
