e08f09fb8ad60a6fae4eb68863dad0bce37511e9
A. Hoffa <60316298+anhoffa@users.noreply.github.com>
2024-11-29 19:51:23 +0100

    Update the theme colors

--- web/static/ux/components/Drawer.mjs
+++ web/static/ux/components/Drawer.mjs
@@ -1,9 +1,9 @@
 import { html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js';
 import { StyledLitElement } from '/ux/StyledLitElement.mjs';
 
-class DrawerBase extends StyledLitElement {
+class Drawer extends StyledLitElement {
   static properties = {
-    anchor: { type: 'left' | 'right' | 'top' | 'bottom' },
+    anchor: { type: 'left' | 'right' | 'top' | 'bottom', reflect: true },
     isOpen: { type: Boolean, reflect: true },
     label: { type: String },
     onClose: { type: Function, attribute: false },
@@ -11,8 +11,9 @@ class DrawerBase extends StyledLitElement {
 
   constructor() {
     super();
-    this.isOpen = true; //todo
-    this.label = 'DrawerBase';
+    this.anchor = 'right';
+    this.isOpen = true;
+    this.label = 'Drawer';
     this.onClose = null;
   }
 
@@ -44,7 +45,7 @@ class DrawerBase extends StyledLitElement {
   render() {
     return html`
       <div>
-        <button class="open-btn" @click=${this.handleToggle}>Cluster Tasks</button>
+        <button class="open-btn" @click=${this.handleToggle}>${this.label}</button>
         <dialog
           class="dialog"
           aria-label=${this.label}
@@ -54,9 +55,9 @@ class DrawerBase extends StyledLitElement {
           <div class="dialog-header">
             <slot name="header"></slot>
             <button class="close-btn" @click=${this.handleClose}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+              </svg>
             </button>
           </div>
           <div class="dialog-content">
@@ -69,45 +70,14 @@ class DrawerBase extends StyledLitElement {
 }
 
 // todo: fix the jumping width issue
-DrawerBase.styles = [
+Drawer.styles = [
   css`
-    dialog {
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      right: 0;
-      left: auto;
-      width: 40rem; // todo
-      min-height: 100vh;
-      max-height: 100vh;
-      padding: 1rem;
-      border: 0;
-      background-color: #2e2e33;
-      color: var(--color-text-primary);
-      box-shadow: -8px 0 20px 4px #242428;
-
-      overflow-y: auto;
-    }
-
-    .dialog-header {
-      display: flex;
+    :host([isOpen]) .open-btn {
+      visibility: hidden;
     }
     
-    .dialog-content {
-      //max-height: 100%;
-      //overflow-y: auto;
-    }
-    
-    *::slotted([name="header"]) {
-      border: 2px solid red;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
-
-    ::slotted([name="content"]) {
-      border: 2px solid red;
-      //overflow: scroll;
+    :host([anchor]) .dialog {
+      // todo
     }
     
     .open-btn {
@@ -122,24 +92,59 @@ DrawerBase.styles = [
       padding: 0.75rem 1.2rem;
 
       &:hover, &:active {
+        cursor: pointer;
         background-color: var(--color-secondary-main);
       }
     }
     
-    .close-btn {
-      all: unset;
-      position: absolute;
-      top: 2rem;
-      right: 2rem;
-      background: transparent;
+    dialog {
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      left: auto;
+      width: 40rem;
+      min-height: 100vh;
+      max-height: 100vh;
+      padding: 1rem;
+      border: 0;
+      background-color: var(--color-fg);
       color: var(--color-text-primary);
+      box-shadow: -8px 0 20px 4px var(--color-shadow-main);
+      overflow-y: auto;
 
-      &:hover, &:active {
-        color: var(--color-text-primary);
-        opacity: 0.8;
+      .dialog-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: start;
+        max-width: inherit;
+
+        ::slotted(*) {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+
+        .close-btn {
+          all: unset;
+          margin-left: 1.5rem;
+          background: transparent;
+          color: var(--color-text-primary);
+
+          &:hover, &:active {
+            cursor: pointer;
+            color: var(--color-text-primary);
+            opacity: 0.8;
+          }
+        }
+      }
+
+      .dialog-content {
+        //max-height: 100%;
+        //overflow-y: auto;
       }
     }
   `
 ]
 
-customElements.define('ui-drawer-base', DrawerBase);
+customElements.define('ui-drawer', Drawer);
