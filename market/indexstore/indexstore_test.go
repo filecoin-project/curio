b21112754af7c9d054709a58c4de68c0f84f4882
Łukasz Magiera <magik6k@gmail.com>
2024-12-09 16:18:05 +0100

    fix indexstore test

--- market/indexstore/indexstore_test.go
+++ market/indexstore/indexstore_test.go
@@ -111,7 +111,7 @@ func TestNewIndexStore(t *testing.T) {
 	pcids, err := idxStore.PiecesContainingMultihash(ctx, m)
 	require.NoError(t, err)
 	require.Len(t, pcids, 1)
-	require.Equal(t, pcids[0], commp.PieceCID)
+	require.Equal(t, pcids[0], commp)
 
 	// Remove all indexes from the store
 	err = idxStore.RemoveIndexes(ctx, pcids[0].PieceCid)
