91ffe2d965a17776229c06291a48407b80f33f2c
Łukasz Magiera <magik6k@gmail.com>
2025-01-02 15:54:52 +0100

    fix blst checkout

--- extern/supraseal/build.sh
+++ extern/supraseal/build.sh
@@ -165,8 +165,9 @@ if [ ! -d "deps/sppark" ]; then
     git clone --branch v0.1.10 https://github.com/supranational/sppark.git deps/sppark
 fi
 if [ ! -d "deps/blst" ]; then
-    git clone --branch bef14ca512ea575aff6f661fdad794263938795d https://github.com/supranational/blst.git deps/blst
+    git clone https://github.com/supranational/blst.git deps/blst
     (cd deps/blst
+    git checkout bef14ca512ea575aff6f661fdad794263938795d
      ./build.sh -march=native)
 fi
 
