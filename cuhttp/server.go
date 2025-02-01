6cf8a81c860148466f2788f01cf9ceb8b24f76f3
zenground0 <ZenGround0@users.noreply.github.com>
2025-02-01 09:03:42 +0530

    Try this

--- cuhttp/server.go
+++ cuhttp/server.go
@@ -2,7 +2,6 @@ package cuhttp
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
 	"net/http"
 	"strings"
@@ -173,9 +172,7 @@ func StartHTTPServer(ctx context.Context, d *deps.Deps, sd *ServiceDeps) error {
 			HostPolicy: autocert.HostWhitelist(cfg.DomainName),
 		}
 
-		server.TLSConfig = &tls.Config{
-			GetCertificate: certManager.GetCertificate,
-		}
+		server.TLSConfig = certManager.TLSConfig()
 		log.Infof("server.TLSConfig NextProtos: %v", server.TLSConfig.NextProtos)
 	}
 
