94bb19ad18b281811b64b0cfe799d10d2415f107
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2025-02-10 15:49:15 +0400

    fix commit param count (#406)

--- tasks/seal/task_submit_commit.go
+++ tasks/seal/task_submit_commit.go
@@ -132,8 +132,8 @@ func (s *SubmitCommitTask) Do(taskID harmonytask.TaskID, stillOwned func() bool)
 		return false, xerrors.Errorf("getting sector params: %w", err)
 	}
 
-	if len(sectorParamsArr) == 0 || (len(sectorParamsArr) < 4 && len(sectorParamsArr) != 1) {
-		return false, xerrors.Errorf("expected either 1 or at least 4 sector params, got %d", len(sectorParamsArr))
+	if len(sectorParamsArr) == 0 {
+		return false, xerrors.Errorf("expected at least 1 sector params, got 0")
 	}
 
 	maddr, err := address.NewIDAddress(uint64(sectorParamsArr[0].SpID))
