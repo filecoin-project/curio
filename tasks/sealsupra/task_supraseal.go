c7e3080394d600b906a7ad8150b18b8ad57e2583
CryptoWhizzard <36404435+cryptowhizzard@users.noreply.github.com>
2025-02-13 12:03:54 +0100

    Fixes SDR tasks in queue , oldest tasks should go first.

--- tasks/sealsupra/task_supraseal.go
+++ tasks/sealsupra/task_supraseal.go
@@ -227,7 +227,7 @@ func (s *SupraSeal) Do(taskID harmonytask.TaskID, stillOwned func() bool) (done 
 		RegSealProof int64 `db:"reg_seal_proof"`
 	}
 
-	err = s.db.Select(ctx, &sectors, `SELECT sp_id, sector_number, reg_seal_proof FROM sectors_sdr_pipeline WHERE task_id_sdr = $1 AND task_id_tree_r = $1 AND task_id_tree_c = $1 AND task_id_tree_d = $1`, taskID)
+	err = s.db.Select(ctx, &sectors, `SELECT sp_id, sector_number, reg_seal_proof FROM sectors_sdr_pipeline WHERE task_id_sdr = $1 AND task_id_tree_r = $1 AND task_id_tree_c = $1 AND task_id_tree_d = $1 ORDER BY create_time ASC`, taskID)
 	if err != nil {
 		return false, xerrors.Errorf("getting sector params: %w", err)
 	}
