9c8eafd39daebac662c5b6c3c72dd94dde3ac52d
Rod Vagg <rod@vagg.org>
2025-06-27 17:05:08 +1000

    fix(chain): improve chain scheduler robustness and eth tx monitoring
    - Add mutex protection and notification timeout handling to CurioChainSched
    - Implement timeout protection for eth client calls to prevent indefinite blocking
    - Add comprehensive test coverage for chain scheduler and eth message watcher
    - Extract interfaces (EthClient, TaskEngine, EthTransactionManager) for testability

--- tasks/message/watch_eth.go
+++ tasks/message/watch_eth.go
@@ -7,37 +7,62 @@ import (
 	"fmt"
 	"math/big"
 	"sync/atomic"
+	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 
 	"github.com/filecoin-project/curio/harmony/harmonydb"
 	"github.com/filecoin-project/curio/harmony/harmonytask"
+	"github.com/filecoin-project/curio/harmony/resources"
 	"github.com/filecoin-project/curio/lib/chainsched"
 
 	types2 "github.com/filecoin-project/lotus/chain/types"
 )
 
+const (
+	// defaultEthCallTimeout is the timeout for sets of Ethereum client calls per transaction
+	// (i.e. receipt and transaction data)
+	defaultEthCallTimeout = time.Second
+)
+
+// EthClient is an interface for the Ethereum client operations we need
+type EthClient interface {
+	TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error)
+	TransactionByHash(ctx context.Context, txHash common.Hash) (tx *types.Transaction, isPending bool, err error)
+	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
+}
+
+// TaskEngine is an interface for the parts of harmonytask.TaskEngine we need
+type TaskEngine interface {
+	ResourcesAvailable() resources.Resources
+}
+
 type MessageWatcherEth struct {
-	db  *harmonydb.DB
-	ht  *harmonytask.TaskEngine
-	api *ethclient.Client
+	txMgr EthTransactionManager
+	ht    TaskEngine
+	api   EthClient
 
 	stopping, stopped chan struct{}
 
 	updateCh        chan struct{}
 	bestBlockNumber atomic.Pointer[big.Int]
+
+	// ethCallTimeout is the timeout for Ethereum client calls
+	ethCallTimeout time.Duration
 }
 
 func NewMessageWatcherEth(db *harmonydb.DB, ht *harmonytask.TaskEngine, pcs *chainsched.CurioChainSched, api *ethclient.Client) (*MessageWatcherEth, error) {
 	mw := &MessageWatcherEth{
-		db:       db,
-		ht:       ht,
-		api:      api,
-		stopping: make(chan struct{}),
-		stopped:  make(chan struct{}),
-		updateCh: make(chan struct{}, 1),
+		txMgr:          NewHarmonyEthTxManager(db),
+		ht:             ht,
+		api:            api,
+		stopping:       make(chan struct{}),
+		stopped:        make(chan struct{}),
+		updateCh:       make(chan struct{}, 1),
+		ethCallTimeout: defaultEthCallTimeout,
 	}
 	go mw.run()
 	if err := pcs.AddHandler(mw.processHeadChange); err != nil {
@@ -80,7 +105,7 @@ func (mw *MessageWatcherEth) update() {
 
 	// Assign pending transactions with null owner to ourselves
 	{
-		n, err := mw.db.Exec(ctx, `UPDATE message_waits_eth SET waiter_machine_id = $1 WHERE waiter_machine_id IS NULL AND tx_status = 'pending'`, machineID)
+		n, err := mw.txMgr.AssignPendingToMachine(ctx, int64(machineID))
 		if err != nil {
 			log.Errorf("failed to assign pending transactions: %+v", err)
 			return
@@ -91,32 +116,37 @@ func (mw *MessageWatcherEth) update() {
 	}
 
 	// Get transactions assigned to us
-	var txs []struct {
-		TxHash string `db:"signed_tx_hash"`
-	}
-
-	err := mw.db.Select(ctx, &txs, `SELECT signed_tx_hash FROM message_waits_eth WHERE waiter_machine_id = $1 AND tx_status = 'pending' LIMIT 10000`, machineID)
+	txHashes, err := mw.txMgr.GetPendingForMachine(ctx, int64(machineID))
 	if err != nil {
 		log.Errorf("failed to get assigned transactions: %+v", err)
 		return
 	}
 
-	log.Infow("Processing pending transactions", "count", len(txs), "machineID", machineID)
+	log.Infow("Processing pending transactions", "count", len(txHashes), "machineID", machineID)
 
 	// Track statistics
 	var processed, stillPending, waitingConfirmations, confirmed, errorCount int
 
 	// Check if any of the transactions we have assigned are now confirmed
-	for _, tx := range txs {
-		txHash := common.HexToHash(tx.TxHash)
+	for _, txHashStr := range txHashes {
+		txHash := common.HexToHash(txHashStr)
 		log.Debugw("Checking transaction", "txHash", txHash.Hex())
 
-		receipt, err := mw.api.TransactionReceipt(ctx, txHash)
+		ethCtx, ethCancel := context.WithTimeout(ctx, mw.ethCallTimeout)
+
+		receipt, err := mw.api.TransactionReceipt(ethCtx, txHash)
 		if err != nil {
+			ethCancel()
 			if errors.Is(err, ethereum.NotFound) {
 				// Transaction is still pending
 				stillPending++
 				log.Debugw("Transaction still pending", "txHash", txHash.Hex())
+				continue
+			}
+			if errors.Is(err, context.DeadlineExceeded) {
+				errorCount++
+				log.Debugw("Eth calls timed out - continuing with next tx",
+					"txHash", txHash.Hex())
 				continue
 			}
 			errorCount++
@@ -124,7 +154,6 @@ func (mw *MessageWatcherEth) update() {
 				"txHash", txHash.Hex(),
 				"error", err,
 				"errorType", fmt.Sprintf("%T", err))
-			// Continue processing other transactions instead of returning
 			continue
 		}
 
@@ -137,6 +166,7 @@ func (mw *MessageWatcherEth) update() {
 		// Check if the transaction has enough confirmations
 		confirmations := new(big.Int).Sub(bestBlockNumber, receipt.BlockNumber)
 		if confirmations.Cmp(big.NewInt(MinConfidence)) < 0 {
+			ethCancel()
 			// Not enough confirmations yet
 			waitingConfirmations++
 			log.Debugw("Transaction waiting for confirmations",
@@ -147,12 +177,20 @@ func (mw *MessageWatcherEth) update() {
 			continue
 		}
 
-		// Get the transaction data
-		txData, _, err := mw.api.TransactionByHash(ctx, txHash)
+		// Get the transaction data using same timeout context
+		txData, _, err := mw.api.TransactionByHash(ethCtx, txHash)
+		ethCancel()
+
 		if err != nil {
 			if errors.Is(err, ethereum.NotFound) {
 				errorCount++
 				log.Errorw("Transaction data not found - continuing", "txHash", txHash.Hex())
+				continue
+			}
+			if errors.Is(err, context.DeadlineExceeded) {
+				errorCount++
+				log.Debugw("Eth calls timed out - continuing with next tx",
+					"txHash", txHash.Hex())
 				continue
 			}
 			errorCount++
@@ -180,7 +218,6 @@ func (mw *MessageWatcherEth) update() {
 			continue
 		}
 
-		txStatus := "confirmed"
 		txSuccess := receipt.Status == 1
 
 		log.Infow("Updating transaction to confirmed",
@@ -190,22 +227,13 @@ func (mw *MessageWatcherEth) update() {
 			"confirmations", confirmations)
 
 		// Update the database
-		_, err = mw.db.Exec(ctx, `UPDATE message_waits_eth SET
-                waiter_machine_id = NULL,
-                confirmed_block_number = $1,
-                confirmed_tx_hash = $2,
-                confirmed_tx_data = $3,
-                tx_status = $4,
-                tx_receipt = $5,
-                tx_success = $6
-                WHERE signed_tx_hash = $7`,
+		err = mw.txMgr.UpdateToConfirmed(ctx,
+			txHashStr,
 			receipt.BlockNumber.Int64(),
 			receipt.TxHash.Hex(),
 			txDataJSON,
-			txStatus,
 			receiptJSON,
 			txSuccess,
-			tx.TxHash,
 		)
 		if err != nil {
 			errorCount++
@@ -226,7 +254,7 @@ func (mw *MessageWatcherEth) update() {
 		"stillPending", stillPending,
 		"waitingConfirmations", waitingConfirmations,
 		"errors", errorCount,
-		"total", len(txs))
+		"total", len(txHashes))
 }
 
 func (mw *MessageWatcherEth) Stop(ctx context.Context) error {
