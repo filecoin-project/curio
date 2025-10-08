f3c19d839b19d13441f438ff7add2f35f8fa6ab8
LexLuthr <lexluthr@curiostorage.org>
2025-10-08 17:28:20 +0400

    fix record size

--- tasks/indexing/task_pdp_ipni.go
+++ tasks/indexing/task_pdp_ipni.go
@@ -17,6 +17,7 @@ import (
 	"github.com/ipni/go-libipni/metadata"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-varint"
 	"github.com/oklog/ulid"
 	"github.com/yugabyte/pgx/v5"
 	"golang.org/x/sync/errgroup"
@@ -264,7 +265,11 @@ func (P *PDPIPNITask) Do(taskID harmonytask.TaskID, stillOwned func() bool) (don
 		eg.Go(func() error {
 			defer close(addFail)
 			for rec := range recs {
-				serr := chk.Accept(rec.Cid.Hash(), int64(rec.Offset), rec.Size)
+				// CAR sections are [varint (length), CID, blockData]
+				combinedSize := rec.Size + uint64(rec.Cid.ByteLen())
+				lenSize := uint64(varint.UvarintSize(combinedSize))
+				sectionSize := combinedSize + lenSize
+				serr := chk.Accept(rec.Cid.Hash(), int64(rec.Offset), sectionSize)
 				if serr != nil {
 					addFail <- struct{}{}
 					return serr
