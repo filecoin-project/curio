28e8954ca217907f7c1a4390d7626caa059c5830
Rod Vagg <rod@vagg.org>
2025-10-11 22:31:30 +1100

    fixup! feat(pdp): add /piece/{cid}/status to get indexing & ipni status for piece

--- pdp/handlers.go
+++ pdp/handlers.go
@@ -191,8 +191,8 @@ func (p *PDPService) handleGetPieceStatus(w http.ResponseWriter, r *http.Request
 			pp.piece_raw_size,
 			pr.created_at,
 
-			-- Indexing status (only true when IPNI ads exist, indicating actual indexing completion)
-			i.ad_cid IS NOT NULL as indexed,
+			-- Indexing status (true when CAR indexing completed and ready for/in IPNI)
+			(pr.needs_ipni OR pr.ipni_task_id IS NOT NULL OR i.ad_cid IS NOT NULL) as indexed,
 
 			-- Advertisement status
 			i.ad_cid IS NOT NULL as advertised,
