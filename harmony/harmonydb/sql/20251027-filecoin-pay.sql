d607edd3fb0d2c08089edccb49313d707c270681
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2025-11-07 17:24:35 +0400

    feat: piece deletion and cleanup (#771)
    * piece deletion and cleanup
    
    * use join
    
    * fix IPNI cleanup
    
    * add piece_cid index to pdp_piecerefs table
    
    * fix args
    
    * more conditional checks
    
    * move delete lower
    
    * fix deletion logic
    
    * delete conditionally

--- harmony/harmonydb/sql/20251027-filecoin-pay.sql
+++ harmony/harmonydb/sql/20251027-filecoin-pay.sql
@@ -17,4 +17,10 @@ CREATE TABLE IF NOT EXISTS pdp_delete_data_set (
     delete_tx_hash TEXT,
 
     terminated BOOLEAN NOT NULL DEFAULT FALSE
-)
+);
+
+ALTER TABLE pdp_data_set_pieces ADD COLUMN rm_message_hash TEXT DEFAULT NULL;
+
+ALTER TABLE pdp_data_set_pieces ADD COLUMN removed BOOLEAN DEFAULT FALSE;
+
+CREATE INDEX IF NOT EXISTS pdp_piecerefs_piece_cid_idx ON pdp_piecerefs (piece_cid);
