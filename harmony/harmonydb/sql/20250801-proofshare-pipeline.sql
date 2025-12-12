6dc4b3f53462d173cd6f4a29a2d6c7bb812c96b1
Andrew Jackson (Ajax) <snadrus@gmail.com>
2025-12-12 16:12:14 -0600

    1

--- harmony/harmonydb/sql/20250801-proofshare-pipeline.sql
+++ harmony/harmonydb/sql/20250801-proofshare-pipeline.sql
@@ -9,10 +9,20 @@ CREATE TABLE IF NOT EXISTS proofshare_client_sender
 
 ALTER TABLE proofshare_client_requests ADD COLUMN IF NOT EXISTS request_type TEXT NOT NULL DEFAULT 'porep';
 
-ALTER TABLE proofshare_client_requests DROP CONSTRAINT proofshare_client_requests_pkey;
-ALTER TABLE proofshare_client_requests ADD  PRIMARY KEY (sp_id, sector_num, request_type);
+ALTER TABLE proofshare_client_requests DROP CONSTRAINT IF EXISTS proofshare_client_requests_pkey;
 
-ALTER TABLE proofshare_client_requests DROP COLUMN task_id;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.table_constraints 
+        WHERE table_name = 'proofshare_client_requests' 
+        AND constraint_type = 'PRIMARY KEY'
+    ) THEN
+        ALTER TABLE proofshare_client_requests ADD PRIMARY KEY (sp_id, sector_num, request_type);
+    END IF;
+END $$;
+
+ALTER TABLE proofshare_client_requests DROP COLUMN IF EXISTS task_id;
 
 ALTER TABLE proofshare_client_requests ADD COLUMN IF NOT EXISTS task_id_upload BIGINT;
 ALTER TABLE proofshare_client_requests ADD COLUMN IF NOT EXISTS task_id_poll   BIGINT;
