4314ba0e657d536cd74d875ed81afdc4043a7807
RedPanda <redpanda@filoz.org>
2026-01-02 15:41:42 +0530

    ready: foc-localnet

--- lib/multicall/address.go
+++ lib/multicall/address.go
@@ -1,6 +1,9 @@
 package multicall
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ethereum/go-ethereum/common"
 	"golang.org/x/xerrors"
 
@@ -16,6 +19,16 @@ func MultiCallAddress() (common.Address, error) {
 		return common.HexToAddress(MultiCallAddressCalibnet), nil
 	case build.BuildMainnet:
 		return common.HexToAddress(MultiCallAddressMainnet), nil
+	case build.Build2k:
+		multicallAddr := os.Getenv("FOC_CONTRACT_MULTICALL")
+		if multicallAddr == "" {
+			return common.Address{}, xerrors.Errorf("FOC_CONTRACT_MULTICALL environment variable must be set for 2k")
+		}
+		if !common.IsHexAddress(multicallAddr) {
+			return common.Address{}, xerrors.Errorf("FOC_CONTRACT_MULTICALL must be a valid hex address")
+		}
+		fmt.Fprintf(os.Stderr, "[2K] FOC_CONTRACT_MULTICALL=%s\n", multicallAddr)
+		return common.HexToAddress(multicallAddr), nil
 	default:
 		return common.Address{}, xerrors.Errorf("multicall address not set for this network %s", build.BuildTypeString()[1:])
 	}
