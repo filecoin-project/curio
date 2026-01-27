86421dc94ed8d467788a7ad84daeba2f1f1ecb9d
Andrew Jackson (Ajax) <snadrus@gmail.com>
2026-01-27 16:27:49 -0600

    right-size porep mem

--- tasks/seal/task_porep.go
+++ tasks/seal/task_porep.go
@@ -166,23 +166,23 @@ func (p *PoRepTask) CanAccept(ids []harmonytask.TaskID, _ *harmonytask.TaskEngin
 
 func (p *PoRepTask) TypeDetails() harmonytask.TaskTypeDetails {
 	gpu := 1.0
+	mem := uint64(96 << 30)
 	if IsDevnet {
 		gpu = 0
+		mem = 1 << 30
 	}
 	res := harmonytask.TaskTypeDetails{
 		Max:  taskhelp.Max(p.max),
 		Name: "PoRep",
 		Cost: resources.Resources{
-			Cpu:       1,
-			Gpu:       gpu,
-			Ram:       50 << 30, // todo correct value
-			MachineID: 0,
+			Cpu: 1,
+			Gpu: gpu,
+			Ram: mem,
 		},
 		MaxFailures: 10,
 		RetryWait: func(retries int) time.Duration {
 			return min(time.Second<<retries, 2*time.Minute)
 		},
-		Follows: nil,
 	}
 
 	if IsDevnet {
