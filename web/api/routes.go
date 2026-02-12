52f9d6bbabeeb64854fa2ed2c6cc9bc29f3566a9
Reiers <nicklas@filecoin.no>
2026-02-12 21:28:58 +0100

    webui: extend chain connectivity with lotus net summary

--- web/api/routes.go
+++ web/api/routes.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/filecoin-project/curio/deps"
 	"github.com/filecoin-project/curio/web/api/config"
+	netapi "github.com/filecoin-project/curio/web/api/net"
 	"github.com/filecoin-project/curio/web/api/sector"
 	"github.com/filecoin-project/curio/web/api/webrpc"
 )
@@ -14,4 +15,5 @@ func Routes(r *mux.Router, deps *deps.Deps, debug bool) {
 	webrpc.Routes(r.PathPrefix("/webrpc").Subrouter(), deps, debug)
 	config.Routes(r.PathPrefix("/config").Subrouter(), deps)
 	sector.Routes(r.PathPrefix("/sector").Subrouter(), deps)
+	netapi.Routes(r.PathPrefix("/net").Subrouter(), deps)
 }
