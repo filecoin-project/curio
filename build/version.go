39340a78ce70d8284ae4b1ffeb26e1d40cdf72fb
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2026-02-16 19:26:45 +0530

    release v1.27.3-rc1 (#1000)

--- build/version.go
+++ build/version.go
@@ -41,10 +41,10 @@ func BuildTypeString() string {
 }
 
 // Intent: Major.Network.Patch
-var BuildVersionArray = [3]int{1, 27, 2}
+var BuildVersionArray = [3]int{1, 27, 3}
 
 // RC
-var BuildVersionRC = 0
+var BuildVersionRC = 1
 
 // Ex: "1.2.3" or "1.2.3-rcX"
 var BuildVersion string
