e5c9b3ed350cb160e12a69676d6514c3e01ccf84
LexLuthr <lexluthr@curiostorage.org>
2026-03-11 17:58:56 +0530

    fix typo

--- tasks/indexing/task_check_indexes.go
+++ tasks/indexing/task_check_indexes.go
@@ -194,7 +194,7 @@ func (c *CheckIndexesTask) checkIndexing(ctx context.Context, taskID harmonytask
 					PieceSize int64     `db:"piece_size"`
 					Offline   bool      `db:"offline"`
 					CreatedAt time.Time `db:"created_at"`
-					DDO bool `db:"ddo"`
+					DDO       bool      `db:"ddo"`
 				}
 				err = c.db.Select(ctx, &mk12deals, `SELECT
 											  uuid,
@@ -253,7 +253,7 @@ func (c *CheckIndexesTask) checkIndexing(ctx context.Context, taskID harmonytask
 									uuid, sp_id, piece_cid, piece_size, raw_size, offline, created_at,
 									sector, sector_offset, reg_seal_proof,
 									started, after_psd, after_commp, after_find_deal, sealed, complete,
-									indexed, indexing_created_at, indexing_task_id, should_index, id_ddo
+									indexed, indexing_created_at, indexing_task_id, should_index, is_ddo
 								)
 								VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,
 										true, true, true, true, true, true,
