85fdd6ed306be1c503ebf825dee96e577ed2780e
LexLuthr <lexluthr@curiostorage.org>
2026-03-27 22:13:49 +0530

    correct filecoin pin URL

--- deps/config/types.go
+++ deps/config/types.go
@@ -122,7 +122,7 @@ func DefaultCurioConfig() *CurioConfig {
 				},
 				IPNI: IPNIConfig{
 					ServiceURL:         []string{"https://cid.contact", "https://filecoinpin.contact"},
-					DirectAnnounceURLs: []string{"https://cid.contact/ingest/announce", "https://filecoinpin.contact/ingest/announce"},
+					DirectAnnounceURLs: []string{"https://cid.contact/ingest/announce", "https://filecoinpin.contact/announce"},
 				},
 			},
 		},
