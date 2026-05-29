9592962f43729f1833c33cb2d76e2ed5ec2bd468
LexLuthr <lexluthr@curiostorage.org>
2026-05-29 16:32:01 +0400

    use semvers

--- pdp/contract/FWSS/utils.go
+++ pdp/contract/FWSS/utils.go
@@ -1,11 +1,9 @@
 package FWSS
 
 import (
-	"strconv"
-	"strings"
-
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"golang.org/x/mod/semver"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/curio/harmony/harmonydb"
@@ -23,97 +21,20 @@ func EnsureServiceTermination(tx *harmonydb.Tx, dataSetID int64) error {
 	return nil
 }
 
-const clientTerminationNotSupportedVersion = "1.2.0"
+const clientTerminationNotSupportedVersion = "1.2.1"
 
 // SupportsClientTermination reports whether FWSS supports
 // terminateService(uint256,bytes) for client-authorized termination.
 func SupportsClientTermination(opts *bind.CallOpts, serviceAddr common.Address, ethClient ethchain.EthClient) (bool, error) {
-	supported, err := VersionAfter(opts, serviceAddr, ethClient, clientTerminationNotSupportedVersion)
-	if err != nil {
-		return false, err
-	}
-	return supported, nil
-}
-
-func getFWSSVersion(opts *bind.CallOpts, serviceAddr common.Address, ethClient ethchain.EthClient) (string, error) {
 	fwss, err := NewFilecoinWarmStorageServiceCaller(serviceAddr, ethClient)
 	if err != nil {
-		return "", xerrors.Errorf("failed to bind FWSS contract: %w", err)
+		return false, xerrors.Errorf("failed to bind FWSS contract: %w", err)
 	}
+
 	version, err := fwss.VERSION(opts)
-	return version, err
-}
+	if err != nil {
+		return false, xerrors.Errorf("failed to get version: %w", err)
+	}
 
-// VersionAtLeast reports whether the FWSS contract VERSION is greater than or equal to minimum.
-func VersionAtLeast(opts *bind.CallOpts, serviceAddr common.Address, ethClient ethchain.EthClient, minimum string) (bool, error) {
-	version, err := getFWSSVersion(opts, serviceAddr, ethClient)
-	if err != nil {
-		return false, xerrors.Errorf("failed to get FWSS version: %w", err)
-	}
-	return versionStringAtLeast(version, minimum)
-}
-
-// VersionAfter reports whether the FWSS contract VERSION is strictly greater than maximum.
-func VersionAfter(opts *bind.CallOpts, serviceAddr common.Address, ethClient ethchain.EthClient, maximum string) (bool, error) {
-	version, err := getFWSSVersion(opts, serviceAddr, ethClient)
-	if err != nil {
-		return false, xerrors.Errorf("failed to get FWSS version: %w", err)
-	}
-	return versionStringAfter(version, maximum)
-}
-
-func versionStringAtLeast(version, minimum string) (bool, error) {
-	current, err := parseVersion(version)
-	if err != nil {
-		return false, xerrors.Errorf("invalid FWSS version %q: %w", version, err)
-	}
-	required, err := parseVersion(minimum)
-	if err != nil {
-		return false, xerrors.Errorf("invalid minimum FWSS version %q: %w", minimum, err)
-	}
-	for i := range current {
-		if current[i] > required[i] {
-			return true, nil
-		}
-		if current[i] < required[i] {
-			return false, nil
-		}
-	}
-	return true, nil
-}
-
-func versionStringAfter(version, maximum string) (bool, error) {
-	current, err := parseVersion(version)
-	if err != nil {
-		return false, xerrors.Errorf("invalid FWSS version %q: %w", version, err)
-	}
-	required, err := parseVersion(maximum)
-	if err != nil {
-		return false, xerrors.Errorf("invalid maximum FWSS version %q: %w", maximum, err)
-	}
-	for i := range current {
-		if current[i] > required[i] {
-			return true, nil
-		}
-		if current[i] < required[i] {
-			return false, nil
-		}
-	}
-	return false, nil
-}
-
-func parseVersion(version string) ([3]int, error) {
-	parts := strings.Split(version, ".")
-	if len(parts) != 3 {
-		return [3]int{}, xerrors.Errorf("expected major.minor.patch")
-	}
-	var parsed [3]int
-	for i, part := range parts {
-		n, err := strconv.Atoi(part)
-		if err != nil {
-			return [3]int{}, err
-		}
-		parsed[i] = n
-	}
-	return parsed, nil
+	return semver.Compare(version, clientTerminationNotSupportedVersion) > 1, nil
 }
