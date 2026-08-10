47583815fe46f82e26cf543dc4760b8e0578041f
beck-8 <1504068285@qq.com>
2026-08-10 15:44:54 +0800

    can disable PDPv0_DatasetIdx

--- tasks/pdpv0/task_dataset_idx.go
+++ tasks/pdpv0/task_dataset_idx.go
@@ -87,6 +87,7 @@ func (t *DatasetIdxTask) TypeDetails() harmonytask.TaskTypeDetails {
 		},
 		MaxFailures: 3,
 		IAmBored:    harmonytask.SingletonTaskAdder(datasetIdxInterval, t),
+		CanDisable:  true,
 	}
 }
 
