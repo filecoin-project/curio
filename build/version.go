bae64aabeac8ae3fa98863e43dcd5825478ad2e5
zenground0 <ZenGround0@users.noreply.github.com>
2026-08-13 12:23:03 -0600

    Release bump

--- build/version.go
+++ build/version.go
@@ -41,7 +41,7 @@ func BuildTypeString() string {
 }
 
 // Intent: Major.Network.Patch
-var BuildVersionArray = [3]int{1, 28, 3}
+var BuildVersionArray = [3]int{1, 28, 4}
 
 // RC
 var BuildVersionRC = 0
