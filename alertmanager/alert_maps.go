5945e9ea9e5db6e5849d8b6254be303dfa582231
beck <1504068285@qq.com>
2026-08-31 13:20:00 +0800

    chore: remove Name_IPNISync from PingHealthFuncs

--- alertmanager/alert_maps.go
+++ alertmanager/alert_maps.go
@@ -35,6 +35,5 @@ func buildPingHealthFuncs() map[AlertName]AlertFunc {
 		Name_ChainSync:             af[Name_ChainSync],
 		Name_PermanentStorageSpace: af[Name_PermanentStorageSpace],
 		Name_PDPTaskFailures:       af[Name_PDPTaskFailures],
-		Name_IPNISync:              af[Name_IPNISync],
 	}
 }
